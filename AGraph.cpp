d9fbe28e24e855621cb83485bab8f42b8fc62de4
githubdlj <511325085@qq.com>
2015-10-09 22:49:06 +0800

    实现了图的DFS，BFS
    实现了图的DFS，BFS。

--- AGraph.cpp
+++ AGraph.cpp
@@ -50,6 +50,7 @@ typedef struct
 
     int vertexNum, edgeNum;
 }AGraph;
+
 //////////////////////////////////////////////////////////////////////
 // 基本函数的申明
 bool InitGraph(AGraph &G);
@@ -59,6 +60,14 @@ bool DestroyGraph(AGraph &G);
 
 bool PrintGraph(AGraph G);
 
+/********************************************************************
+	Method:    getVertexType
+	Parameter: 
+	Returns:   
+
+	Purpose:   根据顶点信息返回节点编号
+
+*********************************************************************/
 int getVertexNo(AGraph G, char info)
 {
     for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
@@ -73,6 +82,25 @@ int getVertexNo(AGraph G, char info)
 }
 
 //////////////////////////////////////////////////////////////////////
+//  遍历
+int isVisited[MAXSIZE];	//	用于标记节点是否被访问
+
+//	访问单个节点
+void visit(VNode v)
+{
+	printf("%c\t", v.v.info);
+}
+
+//	深度优先遍历
+bool DFS(AGraph G, int v);
+bool DFSTraverse(AGraph G);
+
+// 广度优先搜素
+bool BFS(AGraph G, int v);
+bool BFSTraverse(AGraph G);
+
+//////////////////////////////////////////////////////////////////////
+
 /********************************************************************
 	Method:    InitGraph
 	Parameter: 
@@ -192,6 +220,111 @@ bool PrintGraph(AGraph G)
     return true;
 }
 
+//////////////////////////////////////////////////////////////////////
+/********************************************************************
+	Method:    DFS,DFSTraverse
+	Parameter: 
+	Returns:   
+
+	Purpose:   深度优先遍历
+
+*********************************************************************/
+bool DFS(AGraph G, int v)
+{
+	visit(G.vArr[v]);	//	访问
+	isVisited[v] = 1;
+
+	ENode *pENode = G.vArr[v].firstEdge;
+	while (pENode)	//	类似于二叉树的先序遍历
+	{
+		if (!isVisited[pENode->adjvex])
+		{
+			DFS(G, pENode->adjvex);
+		}
+		pENode = pENode->next;
+	}
+	return true;
+}
+
+bool DFSTraverse(AGraph G)
+{
+	for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+	{
+		isVisited[vertexNo] = 0;
+	}
+
+//	int num = 0;
+	//	对每一个节点进行DFS，如果是连通图，则for循环实际上只会执行一次
+	for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+	{
+		if (!isVisited[vertexNo])
+		{
+			DFS(G, vertexNo);
+		//	num++;	//	num为连通分量数目
+		}	
+	}
+	printf("\n");
+
+	return true;
+}
+
+/********************************************************************
+	Method:    BFS
+	Parameter: 
+	Returns:   
+
+	Purpose:   广度优先搜素
+
+*********************************************************************/
+bool BFS(AGraph G, int v)
+{
+    ENode *pENode;
+
+    //  辅助队列
+    int queue[MAXSIZE];
+    int front = 0, rear = 0;
+
+    queue[rear++] = v;    //  顶点入队列
+   
+    visit(G.vArr[v]);   //  访问，标记
+    isVisited[v] = 1;
+    
+    while (front != rear)   //  队列不空
+    {
+        v = queue[front++]; //  出队列
+        
+        pENode = G.vArr[v].firstEdge;   //  pENode指向第一条边
+        while (pENode)
+        {
+            if (!isVisited[pENode->adjvex])
+            {
+                queue[rear++] = pENode->adjvex; //  入队，访问，标记
+                visit(G.vArr[pENode->adjvex]);  
+                isVisited[pENode->adjvex] = 1;
+            }
+            pENode = pENode->next;
+        }
+    }
+    return true;
+}
+
+bool BFSTraverse(AGraph G)
+{
+    for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+    {
+        isVisited[vertexNo] = 0;
+    }
+    
+    for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+    {
+        if (!isVisited[vertexNo])
+        {
+            BFS(G, vertexNo);
+        }
+    }
+    
+    return true;
+}
 /*
     测试函数
 */
@@ -205,7 +338,10 @@ bool OperationTest()
 
     PrintGraph(G);
 
-    DestroyGraph(G);
+	DFSTraverse(G);
+    BFSTraverse(G);
+
+	DestroyGraph(G);
 
     return true;
 }
