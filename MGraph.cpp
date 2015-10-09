379e2ed530c7c7af07d3d4f1d48db67d1b10bd38
githubdlj <511325085@qq.com>
2015-10-09 22:49:19 +0800

    实现了图的DFS，BFS
    实现了图的DFS，BFS

--- MGraph.cpp
+++ MGraph.cpp
@@ -49,7 +49,24 @@ bool DestroyGraph(MGraph &G);
 bool PrintGraph(MGraph G);
 
 int getVertexNo(MGraph G, char info);		//	根据顶点信息得到其编号
+
 //////////////////////////////////////////////////////////////////////
+//	遍历相关
+bool isVisited[MAXSIZE];    //  标记数组
+
+void visit(VertexType v)
+{
+    printf("%c\t", v.info);
+}
+
+//  深度优先搜索
+bool DFS(MGraph G, int v);
+bool DFSTraverse(MGraph G);
+
+// 广度优先搜素
+bool BFS(MGraph G, int v);
+bool BFSTraverse(MGraph G);
+ //////////////////////////////////////////////////////////////////////
 /********************************************************************
 	Method:    InitGraph
 	Parameter: 
@@ -66,10 +83,11 @@ bool InitGraph(MGraph &G)
 	for (int i = 0; i < MAXSIZE; i++)
 	{
 		for (int j = 0; j < MAXSIZE; j++)
-		{
-			G.e[i][j].weight = INF;
-			G.e[j][i].weight = INF;
+		{	
+            G.e[i][j].weight = INF;
+            G.e[j][i].weight = INF;
 		}
+        G.e[i][i] = 0;
 	}
 	return true;
 }
@@ -158,6 +176,106 @@ bool PrintGraph(MGraph G)
 	return true;
 }
 
+//////////////////////////////////////////////////////////////////////
+/********************************************************************
+	Method:    DFS
+	Parameter: 
+	Returns:   
+
+	Purpose:   深度优先遍历
+
+*********************************************************************/
+bool DFS(MGraph G, int v)
+{
+    visit(G.v[v]);
+    isVisited[v] = 1;
+
+    for (int adjvex = 0; adjvex < G.vertexNum; adjvex++)
+    {
+        //  寻找到邻接点，并且邻接点没有被访问，则访问
+        if ( 0 != G.e[v][adjvex] && INF != G.e[v][adjvex] && !isVisited(adjvex))
+        {
+            DFS(G, adjvex);
+        }
+    }
+    
+    return true;
+}
+
+bool DFSTraverse(MGraph G)
+{
+    for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+    {
+        isVisited[vertexNo] = 0;
+    }
+
+    //	int num = 0;
+    //	对每一个节点进行DFS，如果是连通图，则for循环实际上只会执行一次
+    for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+    {
+        if (!isVisited[vertexNo])
+        {
+            DFS(G, vertexNo);
+            //	num++;	//	num为连通分量数目
+        }	
+    }
+    printf("\n");
+
+    return true;
+}
+
+/********************************************************************
+	Method:    BFS
+	Parameter: 
+	Returns:   
+
+	Purpose:   广度优先搜素，注意，入队的时候进行访问，并标记
+
+*********************************************************************/
+bool BFS(MGraph G, int v)
+{
+    //  辅助队列
+    int queue[MAXSIZE];
+    int front = 0, rear = 0;
+
+    queue[rear++] = v;    //  顶点入队列
+   
+    visit(G.vArr[v]);   //  访问，标记
+    isVisited[v] = 1;
+    
+    while (front != rear)   //  队列不空
+    {
+        v = queue[front++]; //  出队列
+        
+        for (int adjvex = 0; adjvex < G.vertexNum; adjvex++)
+        {
+            //  寻找没有被访问过的邻接点
+            if (INF != G.e[v][adjvex] && 0 != G.e[v][adjvex] && !isVisited[adjvex])
+            {
+                queue[rear++] = adjvex; //  入队，访问，标记
+                visit(G.vArr[adjvex]);  
+                isVisited[adjvex] = 1;
+            }
+        }
+    }
+    return true;
+}
+
+bool BFSTraverse(MGraph G)
+{
+    for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+    {
+        isVisited[vertexNo] = 0;
+    }
+    
+    for (int vertexNo = 0; vertexNo < G.vertexNum; vertexNo++)
+    {
+        if (!isVisited[vertexNo])
+        {
+            BFS(G, vertexNo);
+        }
+    }
+}
 /********************************************************************
 	Method:    OperationTest
 	Parameter: 
@@ -173,6 +291,9 @@ bool OperationTest()
 	InitGraph(G);
 	CreateGraph(G);
 	PrintGraph(G);
+
+    DFSTraverse(G);
+    BFSTraverse(G);
 
 	return true;
 }
